0b83e2893abd0d3b715872ad93b31018c409febe
adityach0uhan <m4with6x@gmail.com>
2023-09-14 19:26:09 +0530

    Pattern Questions

---
+++ Arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int row = 5;
+    int count = 1;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i;j++){
+            cout <<count << " " ;
+            count++;
+        }
+        cout << "  " << endl;
+
+    }
+
+    return 0;
+}
