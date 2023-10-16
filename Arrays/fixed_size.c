2b306972d5ed631c0020aa5768fde78813acfc11
adityach0uhan <m4with6x@gmail.com>
2023-10-16 20:46:09 +0530

    More topics

--- Arrays/fixed_size.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char names[3][9] = {"hello", "worldjjjj"}; // the fitst[3] tells the size and 2nd [9] tells the max number of characters 'h' 'e' 'l' 'l' 'o' 'w' this is 5 char so its fine
-    printf("%s\n", names[0]);
-
-    return 0;
-}
